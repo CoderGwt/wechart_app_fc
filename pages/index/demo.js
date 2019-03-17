dc05ce238e890d7aa5485bb193c159cdb1868c15
CoderGwt <1341680843@qq.com>
2019-03-17 12:11:45 +0800

    index 布局

---
+++ pages/index/demo.js
@@ -0,0 +1,34 @@
+Page({
+    data:{
+        // message: "hell world"
+        user:{
+            name: "小明",
+            age: 12
+        },
+        courses: ["wxml", 'wxss', 'javascript'],
+        a: 12,
+        b: 32,
+        users:[
+            {
+                name: "小明",
+                age: 12,
+                gender: "男"
+            },
+            {
+                name: "小李",
+                age: 18,
+                gender: "男"
+            },
+            {
+                name: "小红",
+                age: 16,
+                gender: "女"
+            },
+            {
+                name: "莉莉",
+                age: 19,
+                gender: "女"
+            },
+        ]
+    }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,52 @@
+Page({
+    data:{
+        "title": "游乐活动",
+        'data': "游乐园",
+
+        "slides":[
+            "https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/slide_1.jpg",
+            "https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/slide_2.jpg",
+            "https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/slide_3.jpg",
+            "https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/slide_4.jpg",
+            "https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/slide_5.jpg"
+        ],
+
+        "items":[
+            {
+                url:"https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/item_1.png",
+                text:"疯狂的红包 不一样的撕名牌 大型家庭亲子户外活动",
+                total:"200",
+                data:"周六",
+                label:"免费"
+            },
+            {
+                url:"https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/item_2.png",
+                text:"疯狂的红包 不一样的撕名牌 大型家庭亲子户外活动",
+                total:"200",
+                data:"周六",
+                label:"免费"
+            },
+            {
+                url:"https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/item_3.png",
+                text:"疯狂的红包 不一样的撕名牌 大型家庭亲子户外活动",
+                total:"200",
+                data:"周六",
+                label:"免费"
+            },
+            {
+                url:"https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/item_4.png",
+                text:"疯狂的红包  大型家庭亲子户外活动",
+                total:"200",
+                data:"周六",
+                label:"免费"
+            },
+            {
+                url:"https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/item_1.png",
+                text:"疯狂的红包 ",
+                total:"200",
+                data:"周六",
+                label:"免费"
+            }
+        ]
+    }
+})
